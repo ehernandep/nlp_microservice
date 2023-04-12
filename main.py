5687afe78a07948d6df06c8d3f6979a21d3d5b3d
ehernandep <ehernandep@eafit.edu.co>
2023-04-12 08:44:13 -0500

    adding fast api tests

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-from mylib.logic import wiki
+from fastapi import FastAPI
+import uvicorn
+from mylib.logic import search_wiki
+from mylib.logic import wiki as wikilogic
+from mylib.logic import phrase as wiki_phrases
 
-print(wiki())
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "wikipedia api call/search or wiki"}
+
+
+@app.get("/search/{value}")
+async def search(value: str):
+    result = search_wiki(value)
+    return {"result": result}
+
+
+@app.get("/wiki/{name}")
+async def wiki(name: str):
+    result = wikilogic(name)
+    return {"result": result}
+
+
+@app.get("/phrase/{name}")
+async def phrase(name: str):
+    result = wiki_phrases(name)
+    return {"result": result}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
--- mylib/logic.py
+++ mylib/logic.py
@@ -1,6 +1,19 @@
 import wikipedia
+from textblob import TextBlob
 
 
 def wiki(name="War Goddess", length=1):
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+
+def search_wiki(name):
+    results = wikipedia.search(name)
+    return results
+
+
+def phrase(name):
+    page = wiki(name)
+    blob = TextBlob(page)
+    phrases = blob.noun_phrases
+    return phrases
